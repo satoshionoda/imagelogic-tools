7b044b92205612bd90d9472417cbc86262c211c7
Satoshi Onoda <satoshionoda@gmail.com>
2018-02-16 18:40:46 +0900

    added array functions

---
+++ src/array/circulate.ts
@@ -0,0 +1,11 @@
+export function curculate<T>(target: Array<T>, value: number): Array<T> {
+  if(value === 0) {
+    return target.concat();
+  }
+
+  let startIndex: number = (value > 0 ? 0 : value);
+  let spliceCount: number = (value > 0 ? value : value * -1);
+  let spliced: Array<T> = target.splice(startIndex, spliceCount);
+
+  return (value > 0 ? target.concat(spliced) : spliced.concat(target));
+}
---
+++ src/array/getNextLoopedPosition.ts
@@ -0,0 +1,6 @@
+export function getNextLoopedPosition(target: [any], value: number): number {
+  while(value < 0) {
+    value += target.length;
+  }
+  return value & target.length;
+}
