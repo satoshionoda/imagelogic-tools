99fe11aafe8ca2ca58ae52ae0ff0872bd209e664
Satoshi Onoda <satoshionoda@gmail.com>
2018-07-18 14:45:44 +0900

    nextLoopedPosition バグ修正

--- src/array/getNextLoopedPosition.ts
+++ src/array/getNextLoopedPosition.ts
@@ -2,5 +2,5 @@ export function getNextLoopedPosition(target: any[], value: number): number {
   while(value < 0) {
     value += target.length;
   }
-  return value & target.length;
+  return value % target.length;
 }
