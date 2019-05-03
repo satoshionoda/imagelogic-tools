36a9bb20b064b9ec84dc469f44b7ca3e121a1025
Satoshi Onoda <satoshionoda@gmail.com>
2019-05-03 21:43:29 +0900

    テストコード追加

--- src/array/circulate.ts
+++ src/array/circulate.ts
@@ -1,11 +1,18 @@
+/**
+ *
+ * @param target
+ * @param value
+ */
 export function circulate<T>(target: Array<T>, value: number): Array<T> {
+  const arr = target.concat();
+
   if(value === 0) {
-    return target.concat();
+    return arr;
   }
 
   let startIndex: number = (value > 0 ? 0 : value);
   let spliceCount: number = (value > 0 ? value : value * -1);
-  let spliced: Array<T> = target.splice(startIndex, spliceCount);
+  let spliced: Array<T> = arr.splice(startIndex, spliceCount);
 
-  return (value > 0 ? target.concat(spliced) : spliced.concat(target));
+  return (value > 0 ? arr.concat(spliced) : spliced.concat(arr));
 }
